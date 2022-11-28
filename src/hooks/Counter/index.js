f043d410c46024cd55fa079b3229d5999db252e2
erica-lee1 <erica-lee1@users.noreply.github.com>
2022-11-28 17:40:18 -0600

    first commit

---
+++ src/hooks/Counter/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Counter() { 
+    const [count, setCount ] = useState(0);
+    //const count = useState(0);
+    //setCount = useState(0);
+
+    const increaseCount = () => setCount(count + 1);
+    const decreaseCount = () => setCount(count - 1);
+
+    return <div className="container">
+        <h2>Counter</h2>
+        <p>Count: {count}</p>
+        <button onClick={increaseCount}>+</button>
+        <button onClick={decreaseCount}>-</button>
+    </div>
+}
+
+export default Counter;
